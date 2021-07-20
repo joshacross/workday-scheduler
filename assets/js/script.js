0d3159cf055fe7892a2da5ad2d9af5ab0be183d4
Killa J <joshuaalexandercross@gmail.com>
2021-07-20 16:24:27 -0500

    added event listener to saveBtn

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,17 +8,18 @@
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
 
-var dateFormat = moment().format('dddd, MMM Do YYYY');
-$('#currentDay').html(dateFormat);
-
 $(document).ready(function() {
-    $('saveBtn').on('click', function() {
+
+    var dateFormat = moment().format('dddd, MMM Do YYYY');
+    $('#currentDay').html(dateFormat);
+
+    $('.saveBtn').on('click', function() {
         var task = $(this).siblings('.task');
         var time = $(this).parent().attr('id');
-
+    
+    
         localStorage.setItem(time, task);
     });
-
 
 $('#8 .task').val(localStorage.getItem('8am'));
 $('#9 .task').val(localStorage.getItem('9am'));
@@ -33,32 +34,123 @@ $('#5 .task').val(localStorage.getItem('5pm'));
 $('#6 .task').val(localStorage.getItem('6pm'));
 
 function workdayScheduler() {
+//  WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
     var currentTime = moment().hour();
 
     $('.list-group').each(function() {
         var timeBlock = parseInt($(this).attr('id').split('hour') [1]);
 
         if (timeBlock < currentTime) {
-            $(this).addClass("past");
-            $(this).removeClass("future");
-            $(this).removeClass("present");
+            $(this).addClass("text-danger");
+            $(this).removeClass("text-success");
+            $(this).removeClass("text-secondary");
         }
         else if (timeBlock === currentTime) {
-            $(this).removeClass("past");
-            $(this).addClass("present");
-            $(this).removeClass("future");
+            $(this).removeClass("text-danger");
+            $(this).addClass("text-success");
+            $(this).removeClass("text-secondary");
         }
         else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
+            $(this).removeClass("text-success");
+            $(this).removeClass("text-danger");
+            $(this).addClass("text-secondary");
             }
         });
     }
 
-    workdayScheduler();
+    $("#task").on("click", "textarea", function() {
+        var time = $(this).parent().attr('id');
+        var text = $(this)
+        .text()
+        .trim();
 
-})
+        var textInput = $("<textarea>")
+        .addClass("form-control")
+        .val(text);
+        $(this).replaceWith(textInput);
+        textInput.trigger("focus");
+        localStorage.setItem(time, text);
+        return task;
+      });
+    
+    $("#task").on("blur", "textarea", function() {
+        var time = $(this).parent().attr('id');
+        // get the textarea's current value/text
+    var text = $(this)
+        .val()
+        .trim();
+    //get the parent ul's id attribute
+    var status = $(this)
+        .closest(".list-group")
+        .attr("id")
+        .replace("list-", "");
+        localStorage.setItem(time, text);
+    
+    $("list-group").on("blur", "textarea", function() {
+        // get the textarea's current value/text
+        var text = $(this)
+            .val()
+            .trim();
+        
+        // get the parent ul's id attribute
+        var status = $(this)
+            .closest(".list-group")
+            .attr("id")
+            .replace("list-", "");
+    
+        // get the task's position in the list of other li elements
+        var index = $(this)
+            .closest(".list-group")
+            .index();
+    
+        timeBlocks[status][index].text = text;
+        saveTimeBlocks();
+    
+    });
+    
+    var saveTimeBlocks = function() {
+        localStorage.setItem('timeBlocks', JSON.stringify(timeBlocks));
+    };
+    
+        //   //get the task's position in the list of other li elements
+          var index = $(this)
+            .closest("#task")
+            .index();
+      
+          tasks[status][index].text = text;
+          saveTasks();
+      
+          //recreate p element
+          var taskP = $("<li>")
+            .addClass("m-1")
+            .text(text);
+      
+          // replace textarea with p element
+          $(this).replaceWith(taskP);
+      });
+    
+    // color code using moment
+    var auditTask = function(taskEl) {
+        // get date from task element
+        var date = $(taskEl).find("span").text().trim();
+      
+        // convert to moment object at 5:00pm
+        var time = moment(date, "L").set("hour", 17);
+      
+        // remove any old classes from the element
+        $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+      
+        // apply new class if task is near/over due date
+        if (moment().isAfter(time)) {
+          $(taskEl).addClass("list-group-item-danger");
+        }
+        else if (Math.abs(moment().diff(time,"days")) <=2) {
+          $(taskEl).addClass("list-group-item-warning");
+        }
+      };
+
+});
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours
 
@@ -172,93 +264,6 @@ function workdayScheduler() {
 
 // save to local storage
 // turns li element into a form text input element
-$(".list-group").on("click", ".time-block", function() {
-    var time = $(this).parent().attr('id');
-    var text = $(this)
-    .text()
-    .trim();
-    var textInput = $("<textarea>")
-    .addClass("form-control")
-    .val(text);
-    $(this).replaceWith(textInput);
-    textInput.trigger("focus");
-    localStorage.setItem(time, text);
-  });
 
-$(".list-group").on("blur", "textarea", function() {
-    var time = $(this).parent().attr('id');
-    // get the textarea's current value/text
-var text = $(this)
-    .val()
-    .trim();
-//get the parent ul's id attribute
-var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
-    localStorage.setItem(time, text);
-
-$(".list-group").on("blur", "textarea", function() {
-    // get the textarea's current value/text
-    var text = $(this)
-        .val()
-        .trim();
-    
-    // get the parent ul's id attribute
-    var status = $(this)
-        .closest(".list-group")
-        .attr("id")
-        .replace("list-", "");
-
-    // get the task's position in the list of other li elements
-    var index = $(this)
-        .closest(".list-group-item")
-        .index();
-
-    timeBlocks[status][index].text = text;
-    saveTimeBlocks();
-
-});
-
-var saveTimeBlocks = function() {
-    localStorage.setItem('timeBlocks', JSON.stringify(timeBlocks));
-};
-
-    //   //get the task's position in the list of other li elements
-      var index = $(this)
-        .closest(".list-group-item")
-        .index();
-  
-      tasks[status][index].text = text;
-      saveTasks();
-  
-      //recreate p element
-      var taskP = $("<li>")
-        .addClass("m-1")
-        .text(text);
-  
-      // replace textarea with p element
-      $(this).replaceWith(taskP);
-  });
-
-// color code using moment
-var auditTask = function(taskEl) {
-    // get date from task element
-    var date = $(taskEl).find("span").text().trim();
-  
-    // convert to moment object at 5:00pm
-    var time = moment(date, "L").set("hour", 17);
-  
-    // remove any old classes from the element
-    $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
-  
-    // apply new class if task is near/over due date
-    if (moment().isAfter(time)) {
-      $(taskEl).addClass("list-group-item-danger");
-    }
-    else if (Math.abs(moment().diff(time,"days")) <=2) {
-      $(taskEl).addClass("list-group-item-warning");
-    }
-  };
 
 // // set interval using javascript
